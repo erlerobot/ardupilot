ef363e6dd6fb44320490c989aa08784f1080049e
imuguruza <inigo.muguruza892gmail.com>
2015-05-14 17:22:27 +0200

    Add first approach

--- ROS/apm_rc/src/apm_rc_main.cpp
+++ ROS/apm_rc/src/apm_rc_main.cpp
@@ -1,4 +1,6 @@
 #include "ros/ros.h"
+//#include "apm_rc/apm_rc.h"
+#include "../../../libraries/RC_Channel/RC_Channel.h"
 #include <AP_HAL.h>
 #include <AP_HAL_Linux.h>
 #include <iostream>
@@ -7,6 +9,26 @@
 
 //const HAL_Linux AP_HAL_Linux;
 const AP_HAL::HAL& hal = AP_HAL_BOARD_DRIVER;
+
+#define CH_1 0
+#define CH_2 1
+#define CH_3 2
+#define CH_4 3
+#define CH_5 4
+#define CH_6 5
+#define CH_7 6
+#define CH_8 7
+#define NUM_CHANNELS 8
+
+static RC_Channel rc_1(CH_1);
+static RC_Channel rc_2(CH_2);
+static RC_Channel rc_3(CH_3);
+static RC_Channel rc_4(CH_4);
+static RC_Channel rc_5(CH_5);
+static RC_Channel rc_6(CH_6);
+static RC_Channel rc_7(CH_7);
+static RC_Channel rc_8(CH_8);
+static RC_Channel *rc = &rc_1;
 
 int main(int argc, char **argv)
 {
@@ -19,11 +41,14 @@ int main(int argc, char **argv)
   //ros::Publisher baro_pub = n.advertise<apm_baro::apm_baro>(topic_name, 1000);
   //apm_baro::apm_baro msg;
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(1);
   while (ros::ok()){
-   
-    ros::spinOnce();
-    loop_rate.sleep();
+    for (int i=0; i<NUM_CHANNELS; i++) {
+        int value = rc[i].control_in;
+	std::cout << value << std::endl;
+	}
+	ros::spinOnce();
+	loop_rate.sleep();
   }  
   return 0;
 }
